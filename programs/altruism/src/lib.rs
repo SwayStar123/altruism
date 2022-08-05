4cf7246141efb05eddaf4b2d619eabee28afc231
SwayStar123 <swayambhanded@gmail.com>
2022-08-05 21:52:12 +0530

    added some instruction comments

--- programs/altruism/src/lib.rs
+++ programs/altruism/src/lib.rs
@@ -10,30 +10,37 @@ declare_id!("3QRDFYBJULeGi75Qon1HpDDRMuN1zqwNU9DQ1sGSSQYc");
 pub mod altruism {
     use super::*;
 
+    // Called at the start to setup all the necessary program accounts
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         instructions::initialize::initialize(ctx)
     }
 
+    // function for anyone to make a altSOL token account
     pub fn create_token_account(ctx: Context<CreateTokenAccount>) -> Result<()> {
         instructions::create_token_account::create_token_account(ctx)
     }
 
+    // function for anyone to deposit SOL and get altSOL (backed by mSOL)
     pub fn mint_tokens(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         instructions::deposit::deposit(ctx, amount)
     }
 
+    // orders a delayed unstake of the mSOL and consecuticely the altSOL
     pub fn order_unstake(ctx: Context<OrderUnstake>) -> Result<()> {
         instructions::order_unstake::order_unstake(ctx)
     }
 
+    // claims the delayed unstake ticket
     pub fn claim_withdrawal(ctx: Context<ClaimWithdrawal>) -> Result<()> {
         instructions::claim_withdrawal::claim_withdrawal(ctx)
     }
 
+    // allows ANYONE to order an unstake of the amount over the deposit amount
     pub fn unstake_donation(ctx: Context<UnstakeDonation>) -> Result<()> {
         instructions::unstake_donation::unstake_donation(ctx)
     }
 
+    // allows ANYONE to claim the excess unstaked amount and donate it
     pub fn claim_donation(ctx: Context<ClaimDonation>) -> Result<()> {
         instructions::claim_donation::claim_donation(ctx)
     }    
